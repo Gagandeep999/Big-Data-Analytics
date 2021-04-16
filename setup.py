7c6ba302260e33b750c1fc08976d6a849cfe94a1
GAGANDEEP SINGH <gagan.rishi999@gmail.com>
2021-04-15 22:47:39 -0400

    adding files to master

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Big Data Analytics project to examine and explore montreal towing data and apply statistical methods to produce results.',
+    author='Gagandeep Singh',
+    license='MIT',
+)
